25d4ba7e1076933025e276734e0b456ecf363087
werb210 <todd.w@boreal.financial>
2025-11-16 13:04:01 -0700

    Remove warn method from logger

--- server/src/utils/logger.ts
+++ server/src/utils/logger.ts
@@ -1,6 +1,6 @@
 // server/src/utils/logger.ts
+
 export const logger = {
   info: (...args: any[]) => console.log("[INFO]", ...args),
-  warn: (...args: any[]) => console.warn("[WARN]", ...args),
   error: (...args: any[]) => console.error("[ERROR]", ...args),
 };
