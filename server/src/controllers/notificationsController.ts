52b61e4794de5ac818c4a08e78a08544287a8706
werb210 <todd.w@boreal.financial>
2025-11-16 13:20:59 -0700

    Refactor notifications controller methods and export

--- server/src/controllers/notificationsController.ts
+++ server/src/controllers/notificationsController.ts
@@ -1,10 +1,13 @@
 // server/src/controllers/notificationsController.ts
 import type { Request, Response } from "express";
-import { notificationsService } from "../services/notificationsService.js";
+import notificationsService from "../services/notificationsService.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
-export const notificationsController = {
-  all: asyncHandler(async (_req: Request, res: Response) => {
-    res.json({ ok: true, data: await notificationsService.all() });
+const notificationsController = {
+  list: asyncHandler(async (_req: Request, res: Response) => {
+    const data = await notificationsService.list();
+    res.json({ ok: true, data });
   }),
 };
+
+export default notificationsController;
