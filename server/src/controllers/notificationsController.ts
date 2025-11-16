16a99e4fff034d47cfbcf6d41ad0bd895a2d5718
werb210 <todd.w@boreal.financial>
2025-11-16 13:37:29 -0700

    Refactor notifications service method names

--- server/src/controllers/notificationsController.ts
+++ server/src/controllers/notificationsController.ts
@@ -1,11 +1,11 @@
 // server/src/controllers/notificationsController.ts
 import type { Request, Response } from "express";
-import notificationsService from "../services/notificationsService.js";
+import { notificationsService } from "../services/notificationsService.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 export const notificationsController = {
   list: asyncHandler(async (_req: Request, res: Response) => {
-    const data = await notificationsService.list();
+    const data = await notificationsService.all();
     res.json({ ok: true, data });
   }),
 };
