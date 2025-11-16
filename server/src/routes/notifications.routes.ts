db40a1ccf7286da0bda7d31c09a3fb9cefba08c5
werb210 <todd.w@boreal.financial>
2025-11-16 13:07:53 -0700

    Update notifications.routes.ts

--- server/src/routes/notifications.routes.ts
+++ server/src/routes/notifications.routes.ts
@@ -1,9 +1,10 @@
 // server/src/routes/notifications.routes.ts
+
 import { Router } from "express";
 import { notificationsController } from "../controllers/notificationsController.js";
 
 const router = Router();
 
-router.get("/", notificationsController.all);
+router.get("/", notificationsController.list);
 
 export default router;
