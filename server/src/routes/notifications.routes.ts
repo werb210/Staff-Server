0e4a8dd3f55a40aa880d2810f0149e628a1b3069
werb210 <todd.w@boreal.financial>
2025-11-16 13:49:17 -0700

    Change notifications route to use 'all' method

--- server/src/routes/notifications.routes.ts
+++ server/src/routes/notifications.routes.ts
@@ -1,8 +1,9 @@
+// server/src/routes/notifications.routes.ts
 import { Router } from "express";
-import notificationsController from "../controllers/notificationsController.js";
+import { notificationsController } from "../controllers/notificationsController.js";
 
 const router = Router();
 
-router.get("/", notificationsController.list);
+router.get("/", notificationsController.all);
 
 export default router;
