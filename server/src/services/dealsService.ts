346074a922b961997a61256075bf40b0247396bc
werb210 <todd.w@boreal.financial>
2025-11-17 12:19:25 -0700

    Update dealsService.ts

--- server/src/services/dealsService.ts
+++ server/src/services/dealsService.ts
@@ -1,13 +1,10 @@
 // server/src/services/dealsService.ts
-import { registry } from "../db/registry.js";
+import db, { registry } from "../db/registry.js";
 
 export const dealsService = {
   async all() {
-    const result = await registry.system.query("SELECT * FROM deals LIMIT 200");
-    return result.rows;
-  },
-  async get(id: string) {
-    const result = await registry.system.query("SELECT * FROM deals WHERE id = $1", [id]);
-    return result.rows[0] || null;
+    return db.select().from(registry.deals);
   },
 };
+
+export default dealsService;
