7706938c156b1b05d61f8dbc5423077eaac41317
werb210 <todd.w@boreal.financial>
2025-11-17 11:36:54 -0700

    Update notificationsController.ts

--- server/src/controllers/notificationsController.ts
+++ server/src/controllers/notificationsController.ts
@@ -1,12 +1,11 @@
 // server/src/controllers/notificationsController.ts
-
 import type { Request, Response } from "express";
-import { notificationsService } from "../services/notificationsService.js";
+import notificationsService from "../services/notificationsService.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 export const notificationsController = {
   list: asyncHandler(async (_req: Request, res: Response) => {
-    const rows = await notificationsService.all();
-    res.json({ ok: true, data: rows });
+    const data = await notificationsService.list();
+    res.json({ ok: true, data });
   }),
 };
