12e0f3901ee0252f611a9a7902663a7de5fdbc02
werb210 <todd.w@boreal.financial>
2025-11-17 12:18:42 -0700

    Refactor applicationService to use db.select
    Refactor applicationService to use db.select for querying applications.

--- server/src/services/applicationService.ts
+++ server/src/services/applicationService.ts
@@ -1,14 +1,10 @@
 // server/src/services/applicationService.ts
-import { registry } from "../db/registry.js";
+import db, { registry } from "../db/registry.js";
 
 export const applicationService = {
   async all() {
-    const result = await registry.system.query("SELECT * FROM applications LIMIT 100");
-    return result.rows;
-  },
-
-  async get(id: string) {
-    const result = await registry.system.query("SELECT * FROM applications WHERE id = $1", [id]);
-    return result.rows[0] || null;
+    return db.select().from(registry.applications);
   },
 };
+
+export default applicationService;
