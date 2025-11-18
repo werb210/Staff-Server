b6f3807639389eb4014b0f2f9a50795d0a2c19b9
werb210 <todd.w@boreal.financial>
2025-11-18 09:11:36 -0700

    Refactor user routes to use userController

--- server/src/routes/users.routes.ts
+++ server/src/routes/users.routes.ts
@@ -1,13 +1,12 @@
-// server/src/routes/users.routes.ts
 import { Router } from "express";
-import { usersController } from "../controllers/usersController.js";
+import { userController } from "../controllers/userController.js";
 
 const router = Router();
 
-router.get("/", usersController.list);
-router.get("/:id", usersController.get);
-router.post("/", usersController.create);
-router.put("/:id", usersController.update);
-router.delete("/:id", usersController.remove);
+router.get("/", userController.list);
+router.get("/:id", userController.get);
+router.post("/", userController.create);
+router.put("/:id", userController.update);
+router.delete("/:id", userController.remove);
 
 export default router;
