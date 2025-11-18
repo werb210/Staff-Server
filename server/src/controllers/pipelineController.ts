1152f2d28c0b61812f45f58f275ae9f5f285f6cb
werb210 <todd.w@boreal.financial>
2025-11-18 12:57:33 -0700

    Refactor pipelineController to use pipelineService

--- server/src/controllers/pipelineController.ts
+++ server/src/controllers/pipelineController.ts
@@ -1,22 +1,13 @@
+// FILE: server/src/controllers/pipelineController.ts
 import { Request, Response } from "express";
-import { prisma } from "../db/prisma.js";
+import pipelineService from "../services/pipelineService.js";
 
-export const pipelineController = {
-  async listStages(req: Request, res: Response) {
-    const stages = await prisma.pipelineStage.findMany({
-      include: { cards: true },
-    });
-    res.json(stages);
-  },
-
-  async move(req: Request, res: Response) {
-    const { cardId, toStageId } = req.body;
-
-    const updated = await prisma.pipelineCard.update({
-      where: { id: cardId },
-      data: { stageId: toStageId },
-    });
-
-    res.json(updated);
-  },
+export const getPipeline = async (_req: Request, res: Response) => {
+  res.json(await pipelineService.getPipeline());
 };
+
+export const moveCard = async (req: Request, res: Response) => {
+  res.json(await pipelineService.moveCard(req.params.id, req.body.stageId));
+};
+
+export default { getPipeline, moveCard };
