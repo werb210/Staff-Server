db8405fc4e6da2b47e2249afa53f1d7aeb80a690
werb210 <todd.w@boreal.financial>
2025-11-18 09:35:01 -0700

    Refactor userService to usersService with updates

--- server/src/services/usersService.ts
+++ server/src/services/usersService.ts
@@ -1,52 +1,24 @@
-import { prisma } from "../db/prisma.js";
-import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
-import { env } from "../utils/env.js";
+// server/src/services/usersService.ts
+import { prisma } from "../db/index.js";
 
-export const userService = {
-  async authenticate(email: string, password: string) {
-    const user = await prisma.user.findUnique({ where: { email } });
-    if (!user) return null;
-
-    const valid = await bcrypt.compare(password, user.passwordHash);
-    if (!valid) return null;
-
-    const token = jwt.sign(
-      { id: user.id, email: user.email, role: user.role },
-      env.jwtSecret,
-      { expiresIn: "7d" }
-    );
-
-    return { token, user };
-  },
-
-  async create(data: any) {
-    const hash = await bcrypt.hash(data.password, 10);
-
-    return prisma.user.create({
-      data: {
-        email: data.email,
-        passwordHash: hash,
-        role: data.role ?? "staff",
-        firstName: data.firstName,
-        lastName: data.lastName,
-      },
-    });
-  },
-
+export const usersService = {
   list() {
     return prisma.user.findMany();
   },
 
-  get(id: string) {
+  get(id) {
     return prisma.user.findUnique({ where: { id } });
   },
 
-  update(id: string, data: any) {
+  create(data) {
+    return prisma.user.create({ data });
+  },
+
+  update(id, data) {
     return prisma.user.update({ where: { id }, data });
   },
 
-  remove(id: string) {
+  delete(id) {
     return prisma.user.delete({ where: { id } });
   },
 };
