61cb97f77d9a51e6d4e8ba21d664b5292f033cb7
werb210 <todd.w@boreal.financial>
2025-11-18 09:37:59 -0700

    Refactor contactsController methods and rename remove

--- server/src/controllers/contactsController.ts
+++ server/src/controllers/contactsController.ts
@@ -1,23 +1,24 @@
+// server/src/controllers/contactsController.ts
 import { contactsService } from "../services/contactsService.js";
 
 export const contactsController = {
-  list: async (_req, res) => {
+  async list(req, res) {
     res.json(await contactsService.list());
   },
 
-  get: async (req, res) => {
+  async get(req, res) {
     res.json(await contactsService.get(req.params.id));
   },
 
-  create: async (req, res) => {
+  async create(req, res) {
     res.json(await contactsService.create(req.body));
   },
 
-  update: async (req, res) => {
+  async update(req, res) {
     res.json(await contactsService.update(req.params.id, req.body));
   },
 
-  remove: async (req, res) => {
-    res.json(await contactsService.remove(req.params.id));
+  async remove(req, res) {
+    res.json(await contactsService.delete(req.params.id));
   },
 };
