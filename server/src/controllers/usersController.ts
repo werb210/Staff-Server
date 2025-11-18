b5a6d32e9ce76b5abd313e43efa74a7bc6e68a90
werb210 <todd.w@boreal.financial>
2025-11-18 12:46:32 -0700

    Refactor usersController to use Prisma for DB access

--- server/src/controllers/usersController.ts
+++ server/src/controllers/usersController.ts
@@ -1,24 +1,37 @@
-// server/src/controllers/usersController.ts
-import { usersService } from "../services/usersService.js";
+import { Request, Response } from "express";
+import { prisma } from "../db/prisma.js";
 
 export const usersController = {
-  async list(req, res) {
-    res.json(await usersService.list());
+  async list(req: Request, res: Response) {
+    const users = await prisma.user.findMany();
+    res.json(users);
   },
 
-  async get(req, res) {
-    res.json(await usersService.get(req.params.id));
+  async get(req: Request, res: Response) {
+    const { id } = req.params;
+    const user = await prisma.user.findUnique({ where: { id } });
+    if (!user) return res.status(404).json({ error: "Not found" });
+    res.json(user);
   },
 
-  async create(req, res) {
-    res.json(await usersService.create(req.body));
+  async create(req: Request, res: Response) {
+    const data = req.body;
+    const user = await prisma.user.create({ data });
+    res.json(user);
   },
 
-  async update(req, res) {
-    res.json(await usersService.update(req.params.id, req.body));
+  async update(req: Request, res: Response) {
+    const { id } = req.params;
+    const user = await prisma.user.update({
+      where: { id },
+      data: req.body,
+    });
+    res.json(user);
   },
 
-  async remove(req, res) {
-    res.json(await usersService.delete(req.params.id));
+  async remove(req: Request, res: Response) {
+    const { id } = req.params;
+    await prisma.user.delete({ where: { id } });
+    res.json({ success: true });
   },
 };
