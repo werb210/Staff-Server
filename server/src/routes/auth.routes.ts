a0dbb5fb74f14da3f098da745f6b78f7ec64c062
werb210 <todd.w@boreal.financial>
2025-11-18 09:34:41 -0700

    Add register route to auth routes

--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -4,6 +4,7 @@ import { authController } from "../controllers/authController.js";
 
 const router = Router();
 
+router.post("/register", authController.register);
 router.post("/login", authController.login);
 
 export default router;
