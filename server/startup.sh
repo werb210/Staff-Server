fe27dc38173adf1ab56fe0147621dc69db0e8549
werb210 <todd.w@boreal.financial>
2025-11-23 19:00:59 +0000

    Remove Azure blocking startup files

--- server/startup.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-echo ">>> Building TypeScript"
-npm run build
-
-echo ">>> Starting server"
-node dist/src/index.js
--- startup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /home/site/wwwroot
-npm ci --omit=dev
-node dist/index.js
