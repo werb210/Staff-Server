7d4dd8742063d5917077a23a943d2f28c6db6047
werb210 <todd.w@boreal.financial>
2025-12-02 09:18:07 -0700

    Update users.routes.ts

--- server/src/routes/users.routes.ts
+++ server/src/routes/users.routes.ts
@@ -1,11 +1,10 @@
-// server/src/routes/users.routes.ts
 import { Router } from "express";
-import { usersController } from "../controllers/usersController.js";
+import usersController from "../controllers/usersController.js";
 
 const router = Router();
 
 router.get("/", usersController.list);
-router.get("/:id", usersController.get);
+router.get("/:id", usersController.getOne);
 router.post("/", usersController.create);
 router.put("/:id", usersController.update);
 router.delete("/:id", usersController.remove);
