3dacf50e4b348f02520c21c26abb5aed30faedc4
werb210 <todd.w@boreal.financial>
2025-12-02 09:29:05 -0700

    Update bankingController.ts

--- server/src/controllers/bankingController.ts
+++ server/src/controllers/bankingController.ts
@@ -1,12 +1,24 @@
 import { Request, Response } from "express";
-import bankingRepo from "../db/repositories/bankingAnalysis.repo.js";
 import asyncHandler from "../utils/asyncHandler.js";
+import bankingAnalysisRepo from "../db/repositories/bankingAnalysis.repo.js";
 
 export const bankingController = {
-  getForApp: asyncHandler(async (req: Request, res: Response) => {
+  runAnalysis: asyncHandler(async (req: Request, res: Response) => {
     const { applicationId } = req.params;
-    const entries = await bankingRepo.findMany({ applicationId });
-    res.json(entries);
+
+    const created = await bankingAnalysisRepo.create({
+      applicationId,
+      data: req.body ?? {},
+    });
+
+    res.json(created);
+  }),
+
+  getAnalysis: asyncHandler(async (req: Request, res: Response) => {
+    const { applicationId } = req.params;
+
+    const rows = await bankingAnalysisRepo.findMany({ applicationId });
+    res.json(rows);
   }),
 };
 
