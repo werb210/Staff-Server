1c0c182b3af2899609fa3472532eccf9861b7bee
werb210 <todd.w@boreal.financial>
2025-12-02 09:18:27 -0700

    Update applicationService.ts

--- server/src/services/applicationService.ts
+++ server/src/services/applicationService.ts
@@ -1,24 +1,17 @@
-import { applicationsRepo } from "../db/repositories/applications.repo";
-import { ApplicationInput } from "../types/app";
+import applicationsRepo from "../db/repositories/applications.repo.js";
 
 export const applicationService = {
-  async create(data: ApplicationInput) {
+  create(data: any) {
     return applicationsRepo.create(data);
   },
 
-  async findById(id: string) {
-    return applicationsRepo.findById(id);
-  },
-
-  async update(id: string, data: Partial<ApplicationInput>) {
+  update(id: string, data: any) {
     return applicationsRepo.update(id, data);
   },
 
-  async remove(id: string) {
-    return applicationsRepo.remove(id);
+  get(id: string) {
+    return applicationsRepo.findById(id);
   },
-
-  async listByUser(userId: string) {
-    return applicationsRepo.listByUser(userId);
-  }
 };
+
+export default applicationService;
