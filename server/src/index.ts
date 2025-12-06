2a95c816420282604b28723cdd79f28301b21a89
werb210 <todd.w@boreal.financial>
2025-12-06 10:51:50 -0700

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,9 @@
-import { createServer } from "http";
 import app from "./app";
+import { config } from "dotenv";
+config();
 
 const PORT = process.env.PORT || 5000;
 
-const httpServer = createServer(app);
-
-httpServer.listen(PORT, () => {
-  console.log(`Staff Server running on port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Staff-Server running on port ${PORT}`);
 });
