329c1ef9e56c266bd5d69545fdf2da2892e71577
werb210 <todd.w@boreal.financial>
2025-12-12 15:40:51 -0700

    Update drizzle.config.ts

--- server/drizzle.config.ts
+++ server/drizzle.config.ts
@@ -1,14 +1,10 @@
 import type { Config } from "drizzle-kit";
 
-if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL is missing");
-}
-
 export default {
+  dialect: "postgresql",
   schema: "./src/db/schema/index.ts",
   out: "./drizzle",
-  dialect: "postgresql",
   dbCredentials: {
-    url: process.env.DATABASE_URL,
+    url: process.env.DATABASE_URL!,
   },
 } satisfies Config;
