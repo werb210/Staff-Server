1bdaadfc665895f4cc8adfb5da33907725a63a81
werb210 <todd.w@boreal.financial>
2025-12-12 15:50:29 -0700

    Update drizzle.config.ts

--- server/drizzle.config.ts
+++ server/drizzle.config.ts
@@ -2,9 +2,9 @@ import type { Config } from "drizzle-kit";
 
 export default {
   dialect: "postgresql",
-  schema: "./src/db/schema/index.ts",
+  schema: "./src/db/schema/*.ts",
   out: "./drizzle",
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    url: process.env.DATABASE_URL,
   },
 } satisfies Config;
