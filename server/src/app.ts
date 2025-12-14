107e1c17586feebc363d710f5f47b6b48d1b0924
werb210 <todd.w@boreal.financial>
2025-12-14 12:49:59 -0700

    Update app.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,19 +1,13 @@
 import express from "express";
 import cors from "cors";
 
-import { requestLogger } from "./middleware/requestLogger";
-import { errorHandler } from "./middleware/errorHandler";
-
-import internalRoutes from "./routes/internal";
+import internalRoutes from "./api/internal";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use(requestLogger);
 app.use("/api/_int", internalRoutes);
-
-app.use(errorHandler);
 
 export default app;
