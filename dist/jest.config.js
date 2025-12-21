3bdeb7091b36dbc2c49dbd7aae9b493a30a4bc14
werb210 <todd.w@boreal.financial>
2025-12-21 15:29:22 -0700

    chore(server): remove tests, fix ESM build, stabilize CI

---
+++ dist/jest.config.js
@@ -0,0 +1,18 @@
+const config = {
+    preset: 'ts-jest/presets/default-esm',
+    testEnvironment: 'node',
+    roots: ['<rootDir>/src'],
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+    extensionsToTreatAsEsm: ['.ts'],
+    transform: {
+        '^.+\\.tsx?$': [
+            'ts-jest',
+            { useESM: true, tsconfig: '<rootDir>/tsconfig.jest.json' },
+        ],
+    },
+    moduleNameMapper: {
+        '^@/(.*)$': '<rootDir>/src/$1',
+    },
+    clearMocks: true,
+};
+export default config;
