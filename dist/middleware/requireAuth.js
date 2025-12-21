efd8336307fcb0ca18b12e9c554f949dda67b762
werb210 <todd.w@boreal.financial>
2025-12-21 13:59:39 -0700

    Fix auth route typing and narrow TypeScript build scope

--- dist/middleware/requireAuth.js
+++ dist/middleware/requireAuth.js
@@ -1,6 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.requireAuth = exports.default = void 0;
-var auth_1 = require("./auth");
-Object.defineProperty(exports, "default", { enumerable: true, get: function () { return auth_1.requireAuth; } });
-Object.defineProperty(exports, "requireAuth", { enumerable: true, get: function () { return auth_1.requireAuth; } });
+exports.requireAuth = requireAuth;
+function requireAuth(req, res, next) {
+    if (!req.user) {
+        return res.status(401).json({ error: "Unauthenticated" });
+    }
+    next();
+}
--- dist/routes/users.routes.js
+++ dist/routes/users.routes.js
@@ -4,5 +4,5 @@ const express_1 = require("express");
 const auth_controller_1 = require("../auth/auth.controller");
 const requireAuth_1 = require("../middleware/requireAuth");
 const router = (0, express_1.Router)();
-router.get("/me", requireAuth_1.requireAuth, auth_controller_1.authController.me);
+router.get("/me", requireAuth_1.requireAuth, auth_controller_1.me);
 exports.default = router;
