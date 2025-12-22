6490737d9cad79068417a5f1b2d12146f214bd0f
werb210 <todd.w@boreal.financial>
2025-12-22 13:39:42 -0700

    Update auth.js

--- dist/routes/auth.js
+++ dist/routes/auth.js
@@ -1,1 +1,8 @@
-export { default } from "../auth/auth.routes";
+import { Router } from "express";
+import authRoutes from "../auth/auth.routes.js";
+
+const router = Router();
+
+router.use("/auth", authRoutes);
+
+export default router;
