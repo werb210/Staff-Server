54ca6143aa618d7f62c61c028889067bd608981a
werb210 <todd.w@boreal.financial>
2025-12-23 09:34:37 -0700

    Update password.service.ts

--- server/src/services/password.service.ts
+++ server/src/services/password.service.ts
@@ -1,9 +1,9 @@
-import bcrypt from "bcryptjs";
+import bcrypt from "bcrypt";
 
-export const PASSWORD_ROUNDS = 12;
+const SALT_ROUNDS = 12;
 
 export async function hashPassword(password: string): Promise<string> {
-  return bcrypt.hash(password, PASSWORD_ROUNDS);
+  return bcrypt.hash(password, SALT_ROUNDS);
 }
 
 export async function verifyPassword(
