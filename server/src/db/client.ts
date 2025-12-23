9d9f04809b98d5fc3cd33733bd502e32f5dc4140
werb210 <todd.w@boreal.financial>
2025-12-23 12:19:12 -0700

    Update client.ts

--- server/src/db/client.ts
+++ server/src/db/client.ts
@@ -1,8 +1,5 @@
 import { Pool } from "pg";
-import { drizzle } from "drizzle-orm/node-postgres";
 
-const pool = new Pool({
+export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
-
-export const db = drizzle(pool);
