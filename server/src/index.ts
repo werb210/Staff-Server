b452a8fa7ffd78ad8bddd29326e9552b0d1b4cf2
werb210 <todd.w@boreal.financial>
2025-12-24 09:15:55 -0700

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,18 +1,14 @@
 import express from "express";
 import authRoutes from "./routes/auth.routes.js";
-import usersRoutes from "./api/users/index.js";
 import intRoutes from "./routes/_int.js";
 
 const app = express();
-
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
-app.use("/api/users", usersRoutes);
 app.use("/api/_int", intRoutes);
 
-const port = Number(process.env.PORT) || 5000;
-
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Staff-Server running on port ${port}`);
 });
