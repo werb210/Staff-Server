1ea4ee2e4a503190878de1b5adffa1775cd9d4c3
werb210 <todd.w@boreal.financial>
2025-12-24 09:36:07 -0700

    Update index.ts

--- server/src/api/index.ts
+++ server/src/api/index.ts
@@ -1,10 +1,8 @@
 import { Router } from "express";
 import usersRouter from "./users/index.js";
-import authRouter from "./auth/index.js";
 
 const router = Router();
 
 router.use("/users", usersRouter);
-router.use("/auth", authRouter);
 
 export default router;
