988286529e20fc8a956c222dff91892ca362db0c
werb210 <todd.w@boreal.financial>
2025-12-24 09:10:07 -0700

    Update verify-sms.ts

--- server/src/api/auth/verify-sms.ts
+++ server/src/api/auth/verify-sms.ts
@@ -1,8 +1,14 @@
-import { checkVerificationCode } from "../../services/twilio.service.js";
 import type { Request, Response } from "express";
+import { checkVerificationCode } from "../../services/twilio.service.js";
 
 export async function verifySms(req: Request, res: Response) {
   const { to, code } = req.body;
-  await checkVerificationCode(to, code);
-  res.sendStatus(204);
+
+  if (!to || !code) {
+    return res.status(400).json({ error: "Missing parameters" });
+  }
+
+  const result = await checkVerificationCode(to, code);
+
+  res.json({ result });
 }
