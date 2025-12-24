92456dd01fd932b2896dc90e12dee95fbd4e8ce1
werb210 <todd.w@boreal.financial>
2025-12-24 09:25:38 -0700

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,12 @@
 import express from "express";
-import apiRouter from "./api/index.js";
+import api from "./api/index.js";
 import intRoutes from "./routes/_int.js";
 
 const app = express();
 
 app.use(express.json());
 
-app.use("/api", apiRouter);
+app.use("/api", api);
 app.use("/api/_int", intRoutes);
 
 const port = process.env.PORT || 5000;
