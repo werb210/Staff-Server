740904434a2f8911b1c79989a199a5a438c0b166
werb210 <todd.w@boreal.financial>
2025-12-24 10:54:18 -0700

    Update jwt.service.ts

--- server/src/services/jwt.service.ts
+++ server/src/services/jwt.service.ts
@@ -1,9 +1,7 @@
 import jwt from "jsonwebtoken";
 
-const JWT_SECRET = process.env.JWT_SECRET as string;
-if (!JWT_SECRET) {
-  throw new Error("JWT_SECRET not set");
-}
+const JWT_SECRET = process.env.JWT_SECRET;
+if (!JWT_SECRET) throw new Error("JWT_SECRET not set");
 
 export interface AccessTokenPayload {
   sub: string;
