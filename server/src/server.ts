d901d797b16645bacc89834333981ddedfb99702
werb210 <todd.w@boreal.financial>
2025-12-25 15:45:58 -0700

    Align server entrypoint and health checks

--- server/src/server.ts
+++
@@ -1,8 +0,0 @@
-import app from "./index.js";
-
-const PORT = Number(process.env.PORT) || 8080;
-const HOST = "0.0.0.0";
-
-app.listen(PORT, HOST, () => {
-  console.log(`Staff-Server running on ${HOST}:${PORT}`);
-});
