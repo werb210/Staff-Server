cee04397edf6d0de9a7a3ad50df772ac5d5d99bb
werb210 <todd.w@boreal.financial>
2025-12-27 16:25:18 -0700

    Update client.ts

--- server/src/db/client.ts
+++ server/src/db/client.ts
@@ -1,13 +1,8 @@
 import { Pool } from 'pg';
-import * as schema from './schema';
 
-const pool = new Pool({
+export const client = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: process.env.DATABASE_SSL === 'true'
     ? { rejectUnauthorized: false }
     : undefined,
 });
-
-export const client = pool;
-export default client;
-export { schema };
