d075def614d13e5e7a445f1b564d7e870b9bd3fe
werb210 <todd.w@boreal.financial>
2025-12-28 16:28:36 -0700

    Move runtime dependencies into server

--- server/dist/index.js
+++ server/dist/index.js
@@ -5,6 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
+app.get("/", (_req, res) => res.status(200).send("ok"));
 app.get("/api/_int/health", (_req, res) => res.status(200).send("ok"));
 app.get("/api/_int/live", (_req, res) => res.status(200).send("live"));
 const port = Number(process.env.PORT) || 8080;
