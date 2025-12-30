16b6dd26495330abbf7f9dfc57865b1dc6123f3f
werb210 <todd.w@boreal.financial>
2025-12-30 13:28:41 -0700

    Update api.ts

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,15 +1,14 @@
 import { Router } from "express";
-
+import health from "./health";
 import auth from "./auth";
 import users from "./users";
 import system from "./system";
-import health from "./health";
 
 const router = Router();
 
+router.use("/health", health);
 router.use("/auth", auth);
 router.use("/users", users);
 router.use("/system", system);
-router.use("/health", health);
 
 export default router;
