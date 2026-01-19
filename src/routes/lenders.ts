a7267b2e09392eb5757dd0dae9b02f71083e7441
werb210 <todd.w@boreal.financial>
2026-01-19 11:05:59 -0700

    Use ops manage capability for lender creation

--- src/routes/lenders.ts
+++ src/routes/lenders.ts
@@ -14,7 +14,7 @@ router.use(requireAuth);
 router.get("/", requireCapability([CAPABILITIES.LENDERS_READ]), safeHandler(listLendersHandler));
 router.post(
   "/",
-  requireCapability([CAPABILITIES.LENDERS_WRITE]),
+  requireCapability([CAPABILITIES.OPS_MANAGE]),
   safeHandler(createLenderHandler)
 );
 
