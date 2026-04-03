9b44284d38b0ab8ef98bd83e7e6d2f8639728df5
werb210 <todd.w@boreal.financial>
2026-04-03 14:16:32 -0600

    Run prebuilt dist app on startup and add boot logging

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,3 @@
 require("module-alias/register");
-import { app } from "./app";
-import { initDb } from "@/db/init";
-
-export default app;
-
-if (process.env.NODE_ENV !== "test") {
-  const PORT = Number(process.env.PORT) || 8080;
-  void (async () => {
-    await initDb();
-    app.listen(PORT, "0.0.0.0", () => {
-      console.log(`Server running on port ${PORT}`);
-    });
-  })();
-}
+export { app } from "./app";
+export { default } from "./app";
